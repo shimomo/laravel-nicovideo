c9e18af27a343ed8d1dc448cdc636bf7d3c4d508
shimomo <yuichi@shimomo.net>
2017-02-03 16:18:28 +0900

    Add example

---
+++ examples/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @author shimomo
+ */
+class ExampleController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function test()
+    {
+        $result = \Nicovideo::search([
+            'service' => 'video',
+            'q' => '初音ミク',
+            'targets' => 'title',
+            'fields' => 'contentId,title,viewCounter',
+            'filters[viewCounter][gte]' => '10000',
+            '_sort' => '-viewCounter',
+            '_offset' => 0,
+            '_limit' => 3,
+            '_context' => 'apiguide',
+        ]);
+
+        var_dump($result);
+
+        return 'ok';
+    }
+}
---
+++ examples/readme.md
@@ -0,0 +1,9 @@
+# Laravel-Nicovideo
+
+## Try it out.
+```
+$ git clone https://github.com/shimomo/laravel-nicovideo.git
+$ cd laravel-nicovideo
+$ php artisan serve
+$ curl 127.0.0.1:8000
+```
